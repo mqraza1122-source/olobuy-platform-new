4bb0ca31f11f09299582ab74ff65fb1d0464cefd
mqraza1122-source <mqraza1122@gmail.com>
2026-07-07 04:37:51 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,27 @@
+import { SiteHeader } from '@/components/site-header'
+import { Hero } from '@/components/hero'
+import { HowItWorks } from '@/components/how-it-works'
+import { Features } from '@/components/features'
+import { Testimonials } from '@/components/testimonials'
+import { Faq } from '@/components/faq'
+import { TrustPartners } from '@/components/trust-partners'
+import { CtaFooter } from '@/components/cta-footer'
+import { WhatsAppFloat } from '@/components/whatsapp-float'
+
+export default function HomePage() {
+  return (
+    <div className="flex min-h-dvh flex-col">
+      <SiteHeader />
+      <main className="flex-1">
+        <Hero />
+        <HowItWorks />
+        <Features />
+        <Testimonials />
+        <Faq />
+        <TrustPartners />
+        <CtaFooter />
+      </main>
+      <WhatsAppFloat />
+    </div>
+  )
+}
