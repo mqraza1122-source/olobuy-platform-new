8c49efacf18a17aef35761f1739275154af2ef09
mqraza1122-source <mqraza1122@gmail.com>
2026-07-08 01:54:51 +0500

    Update WhatsApp number in constants

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,8 +1,8 @@
 // Update this number to your business WhatsApp number (international format, no +)
-export const WHATSAPP_NUMBER = '923000000000'
+export const WHATSAPP_NUMBER = '923043031572'
 
 export const WHATSAPP_MESSAGE = encodeURIComponent(
-  "Hi OloBuy! I'd like to start a Safe Deal (escrow) for an item.",
+  "Hi OloBuy! I'd like to start a Safe Deal (escrow) for an item."
 )
 
 export const WHATSAPP_URL = `https://wa.me/${WHATSAPP_NUMBER}?text=${WHATSAPP_MESSAGE}`
