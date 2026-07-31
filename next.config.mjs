95be2af546b19a2a884e10c8a3a8de4f61f94d0e
mqraza1122-source <mqraza1122@gmail.com>
2026-08-01 00:04:51 +0500

    Add ESLint configuration to next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,12 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
