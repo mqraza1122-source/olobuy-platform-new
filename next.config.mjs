8916aa9ef9d800ab3a17c481c417089625392906
mqraza1122-source <mqraza1122@gmail.com>
2026-08-01 00:28:38 +0500

    Update next.config.mjs with new configurations

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+};
+
 export default nextConfig;
